ba0c334cd8117a2927f1fcbe680efed85e6cc1fa
LetteraUnica <lorenzocav97@gmail.com>
2021-09-30 14:12:43 +0200

    Update submit_job.sh

--- pytorch_ca/submit_job.sh
+++ pytorch_ca/submit_job.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#PBS -l nodes=1:ppn=2
+#PBS -l nodes=1:ppn=2,mem=2gb
 #PBS -l walltime=1:00:00
+#PBS -N hyperparam_search
 
+module load conda/4.9.2
 
 cd $PBS_O_WORKDIR
 cd neural_ca/pytorch_ca
