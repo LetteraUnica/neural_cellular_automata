2fdc0887f676e45e509e6208a5246ada563c96ba
LetteraUnica <lorenzocav97@gmail.com>
2021-09-30 12:34:33 +0200

    Update submit_job.sh

--- pytorch_ca/submit_job.sh
+++ pytorch_ca/submit_job.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=4
+#PBS -l mem=2gb
 #PBS -l walltime=1:00:00
 
 
 cd $PBS_O_WORKDIR
 cd neural_ca/pytorch_ca
 
-python3 neural_ca_benchmark.py
+conda activate gpu
+wandb agent neural_ca/NeuralCA/0bavlnat
