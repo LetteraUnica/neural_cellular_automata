b2f089c2b631bac54a9acf690e4203209fc49800
LetteraUnica <lorenzocav97@gmail.com>
2021-09-30 14:21:36 +0200

    Update submit_job.sh

--- pytorch_ca/submit_job.sh
+++ pytorch_ca/submit_job.sh
@@ -3,12 +3,13 @@
 #PBS -l walltime=1:00:00
 #PBS -N hyperparam_search
 
-module load conda/4.9.2
-
 cd $PBS_O_WORKDIR
 cd neural_ca/pytorch_ca
 
+module load conda/4.9.2
+conda init bash
 conda activate gpu
+
 wandb agent neural_ca/NeuralCA/0bavlnat
 
 done
