c1355c6b1a19abf9a2a68cebc28fc3de5292aee2
Francesco215 <47751420+Francesco215@users.noreply.github.com>
2021-12-05 16:36:34 +0100

    fix

--- pytorch_ca/src/utils/loss_utils.py
+++ pytorch_ca/src/utils/loss_utils.py
@@ -1,8 +1,12 @@
+import torch
+import numpy as np
+
+
 class combination_function_generator:
 
     def __init__(self, tau, img_interval , kill_interval, kill_multiplier, device="cpu"):
         self.tau=tau
-        self.img_interval=img_inteval
+        self.img_interval=img_interval
         self.kill_interval=kill_interval
         self.kill_multiplier=kill_multiplier
         self.log_step=img_interval[-1]
@@ -11,6 +15,7 @@ class combination_function_generator:
         self.tau_decay=20
 
     def exponential(self, x, tau, interval):
+        n_steps = x + interval[0]
         if n_steps >= interval[0] and n_steps <= interval[1]:
             return np.exp(tau*x)
         return 0
@@ -33,10 +38,10 @@ class combination_function_generator_growing(combination_function_generator):
 
     def __call__(self, n_steps,n_epoch=0):
         tau=self.tau*np.exp(-n_epoch/self.tau_decay)
-        m1=self.exponential(self.img_interval[1]-n_steps, tau)
+        m1=self.exponential(self.img_interval[1]-n_steps, tau, self.img_interval)
         
         m2=0
         if n_steps >= self.kill_interval[0] and n_steps<= self.kill_interval[1]:
             m2=self.kill_multiplier
         
-        return torch.tensor([m1,m2],device=device).float()
+        return torch.tensor([m1,m2],device=self.device).float()
