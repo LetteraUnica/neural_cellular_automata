ce99877121d53b724cf0f77143e30b7b1c9ba83a
Francesco215 <47751420+Francesco215@users.noreply.github.com>
2021-12-12 12:59:32 +0100

    minor

--- pytorch_ca/src/loss_functions.py
+++ pytorch_ca/src/loss_functions.py
@@ -120,7 +120,7 @@ class CombinedLoss:
 
     def __call__(self, x, n_steps=0, n_epoch=0, initial_step=0) -> torch.Tensor:
         losses = torch.stack([loss(x) for loss in self.losses]).float()
-        return torch.matmul(self.f(n_steps,n_epoch), losses) #This gives problem if some variables are not in cuda
+        return torch.sum(self.f(n_steps,n_epoch)*losses,axis=0) #This gives problem if some variables are not in cuda
 
     def log_loss(self,x:torch.Tensor)->float:
         return self.losses[0](x)
