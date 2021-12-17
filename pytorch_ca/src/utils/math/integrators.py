bcaeb0b737e178841e72391ab36743d0e1d6647e
LetteraUnica <lorenzocav97@gmail.com>
2021-12-17 18:13:55 +0100

    Renamed and refactored class

--- pytorch_ca/src/utils/math/integrators.py
+++ pytorch_ca/src/utils/math/integrators.py
@@ -1,43 +1,24 @@
-from typing import Tuple
+from typing import Sequence
 
-import numpy as np
+from functools import cache
 
 
-class CachedDiscreteIntegrator:
+class CachedSummer:
     """Integrates a function between two positive integer values and keeps the integral cached for faster evaluation"""
 
     def __init__(self, function):
         self.function = function
-        self.integral = []
 
-    @staticmethod
-    def swap(a: int, b: int) -> Tuple[int, int]:
-        """Returns the two elements swapped"""
-        c = a
-        a = b
-        b = c
-
-        return a, b
-
-    def last_value(self):
-        """Returns the last value in the integral"""
-        if len(self.integral) > 0:
-            return self.integral[-1]
-        else:
+    @cache
+    def sum_until(self, n):
+        if n < 0:
             return 0
+        return self.sum_until(n-1) + self.function(n)
 
-    def integrate(self, a: int, b: int) -> float:
-        """Integrates the function between a and b"""
-        if a > b:
-            a, b = self.swap(a, b)
+    def sum_between(self, a: int, b: int) -> float:
+        """Integrates the function in [a, b]"""
+        return self.sum_until(b) - self.sum_until(a-1)
 
-        if a < 0:
-            return 0
-
-        if len(self.integral) < b:
-            new_function_values = [self.function(i) for i in range(len(self.integral), b+1)]
-
-            for value in new_function_values:
-                self.integral.append(value + self.last_value())
-
-        return self.integral[b] - self.integral[a]
+    def sum_between_vectorized(self, a: Sequence[int], b:Sequence[int]) -> Sequence[float]:
+        """Integrates the function between a and b with a and b sequences"""
+        return [self.sum_between(ai, bi) for (ai, bi) in zip(a, b)]
