20975731276f296b1c1283501979c39b01fa9576
LetteraUnica <lorenzocav97@gmail.com>
2021-12-18 16:36:51 +0100

    Simplified vectorization

--- pytorch_ca/src/utils/math/integrators.py
+++ pytorch_ca/src/utils/math/integrators.py
@@ -2,23 +2,24 @@ from typing import Sequence
 
 from functools import cache
 
+import numpy as np
+
 
 class CachedSummer:
     """Integrates a function between two positive integer values and keeps the integral cached for faster evaluation"""
 
     def __init__(self, function):
         self.function = function
+        self.sum_until = np.vectorize(self._sum_until)
+        self.sum_between = np.vectorize(self._sum_between)
 
     @cache
-    def sum_until(self, n):
+    def _sum_until(self, n):
+        """Integrates the function in [0, n]"""
         if n < 0:
             return 0
-        return self.sum_until(n-1) + self.function(n)
+        return self._sum_until(n-1) + self.function(n)
 
-    def sum_between(self, a: int, b: int) -> float:
+    def _sum_between(self, a: int, b: int) -> float:
         """Integrates the function in [a, b]"""
-        return self.sum_until(b) - self.sum_until(a-1)
-
-    def sum_between_vectorized(self, a: Sequence[int], b:Sequence[int]) -> Sequence[float]:
-        """Integrates the function between a and b with a and b sequences"""
-        return [self.sum_between(ai, bi) for (ai, bi) in zip(a, b)]
+        return self._sum_until(b) - self._sum_until(a-1)
