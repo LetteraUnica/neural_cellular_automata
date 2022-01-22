c58609588885e62ddcbb229d44251b262b7c4138
LetteraUnica <lorenzocav97@gmail.com>
2022-01-22 20:46:19 +0100

    Added square_mask

--- pytorch_ca/src/utils/utils.py
+++ pytorch_ca/src/utils/utils.py
@@ -1,3 +1,4 @@
+from typing import Sequence
 import torch
 
 from random import randint
@@ -52,10 +53,21 @@ def make_squares(images, target_size=None, side=side, constant_side=False):
     return images.clone()
 
 
-def checkered_mask(matrix_size):
-    grid = torch.zeros(matrix_size, matrix_size)
-    for i in range(matrix_size):
-        for j in range(matrix_size):
+def checkered_mask(mask_size: int) -> torch.Tensor:
+    mask = torch.zeros(mask_size, mask_size, dtype=int)
+    for i in range(mask_size):
+        for j in range(mask_size):
             if (i+j) % 2 == 0:
-                grid[i, j] = 1
-    return grid.unsqueeze(0).unsqueeze(0)
+                mask[i, j] = 1
+
+    return mask
+
+
+def square_mask(mask_size: int, center: Sequence[int], side: int) -> torch.Tensor:
+    left = center - side // 2
+    right = left + side
+
+    mask = torch.zeros((mask_size, mask_size), dtype=int)
+    mask[left[0]:right[0], left[1]:right[1]] = 1
+    
+    return mask
