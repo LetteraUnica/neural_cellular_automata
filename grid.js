6b8279bcfbad2a958ca4bd0be3b6861f1610d047
Francesco215 <f.sacco@protonmail.com>
2022-01-23 22:45:11 +0100

    it works!

--- grid.js
+++ grid.js
@@ -1,5 +1,5 @@
 var gridData = gridValue;	
-var colors=[["#fff","#fde411","#fd4949","#fef495","#fd9695","#fd8b1f"],["#fff","#fff","#fff","#fff","#fff","#fff"]];
+var colors=[["#fff","#fde411","#fd4949","#fef495","#fd9695","#fd8b1f"],["#fff","#fef0b0","#feb6b6","#fef9cc","#fecccc","#fec8b1"]];
 
 // I like to log the data to the console for quick debugging
 console.log(gridData);
@@ -30,15 +30,15 @@ var column = row.selectAll(".square")
     .on('mouseover', function(d) { //d is the data of the square pointed with the mouse
         d3.selectAll(".square").style("fill",function (x){
             color_selected=d.click%6
-            all_colors=x.click%6
+            all_colors=x.click%6  //x is the data of all the squares
             if (color_selected==0) {
                 for (var i=0;i<6;i++){
                     if (all_colors==i) {return colors[0][i];}
                 }
             }    
-            for (var i=0; i<6;i++){
-                if (color_selected == i && color_selected!=all_colors) { return colors[1][i]; }
-                if ((color_selected == i && color_selected==all_colors)) { return colors[0][i]; }
+            for (var i=0; i<6; i++){
+                if (all_colors==color_selected) {return colors[0][color_selected];}
+                if (all_colors==i) {return colors[1][i];}
             }
         })
     });
@@ -49,16 +49,15 @@ function color_scheme(d){
     
 }
 
-
-
 /*
-.on('mouseover', function(d) {
-        d3.selectAll(".square").style("fill",function (x){
-            for (var i=0; i<6;i++){
-                if (d.click==0) { return colors[0][i]; }
-                if ((d.click)%6 == i  && d.click!=x.click) { return colors[1][i]; } 
-                if ((d.click)%6 == i  && d.click==x.click) {return colors[0][i];}   //white
-            }
-        })
-    });
+for (var i=1; i<6;i++){
+    if(color_selected==i){
+        for (var j=0;j<6;j++){
+            if (all_colors==color_selected) {return colors[0][j];}
+        }
+    for (var j=0;j<6;j++){
+        if (all_colors==j) {return colors[1][j];}
+        }
+    }
+}
 */
