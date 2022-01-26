6e6389f680e52728e05488c74a5cd1db310089d6
Francesco215 <f.sacco@protonmail.com>
2022-01-26 16:57:41 +0100

    added interactive text on mousehover on the graph

--- grid.js
+++ grid.js
@@ -1,9 +1,8 @@
 var colors=[["#fff","#fde411","#fd4949","#fef495","#fd9695","#fd8b1f"],   //base colors
             ["#fff","#fef0b0","#feb6b6","#fef9cc","#fecccc","#fec8b1"],   //dimemd colors colors
             ["#fff","#fde411","#fd4949","#fde411","#fd4949","#fd8b1f"]];  //bright colors
-//var colors=[["#fff","#fde411","#fd4949","#fef495","#fd9695","#fd8b1f"],["#fff","#fdea7d","#fd8a8a","#fef6b1","#fdb2b1","#fdab7f"]];
 
-// I like to log the data to the console for quick debugging
+var text_ids=["white","yellow","red","light_yellow","light_red","orange"];
 
 var image_width=parseInt(document.getElementById("grid_container").clientWidth)
 
@@ -47,12 +46,16 @@ console.log(gridData);
 var article = document.getElementById("frame")
     .addEventListener('mouseover',function(){
         d3.selectAll(".square").style("fill",function (x){
-            for (var i=0; i<6; i++){
-                if (x.click==i) {return colors[0][i];}
+            hightlight(0);
+            for (var i=1;i<6;i++){
+                anti_hightlight(i);
             }
+            return colors[0][x.click];
         })
-        console.log('suca')
     })
+
+
+
 
 var grid = d3.select("#grid")
 	.append("svg")
@@ -81,19 +84,30 @@ var column = row.selectAll(".square")
         }
     })
     .on('mouseover', function(d) { //d is the data of the square pointed with the mouse
-        d3.selectAll(".square").style("fill",function (x){
-            color_selected=d.click
-            all_colors=x.click  //x is the data of all the squares
+        hightlight(d.click);
+        d3.selectAll(".square").style("fill",function (x){//x is the data of all the squares
             if (d.click==0) {
                 for (var i=0;i<6;i++){
-                    if (x.click==i) {return colors[0][i];}
+                    if (x.click==i) {
+                        if (text_ids[i]!="white"){anti_hightlight(i);}
+                        return colors[0][i];
+                    }
                 }
-            }    
-            for (var i=0; i<6; i++){
-                if (x.click==d.click) {return colors[2][color_selected];}
-                if (x.click==i) {return colors[1][i];}
+            }
+            if (x.click==d.click) {return colors[2][d.click];}
+            anti_hightlight(x.click);
+            for (var i=0; i<6; i++){                
+                if (x.click==i) {
+                    return colors[1][i];
+                }
             }
         })
     });
 
+hightlight = function(id){
+    document.getElementById(text_ids[id]).style.color="black";   
+}
+anti_hightlight = function(id){
+    document.getElementById(text_ids[id]).style.color="grey";   
+}
 
