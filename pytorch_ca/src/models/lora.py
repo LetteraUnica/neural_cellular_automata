3d9fe5bfcb8b1b33cc3c21297b755775d75a50b7
Francesco215 <f.sacco@protonamail.com>
2023-10-10 03:28:02 +0000

    written lora convolutional layer

--- pytorch_ca/src/models/lora.py
+++ pytorch_ca/src/models/lora.py
@@ -6,7 +6,9 @@ from torch.nn import functional as F
 class LoraConvLayer(nn.Module):
     def __init__(self, conv_layer:nn.Conv2d, rank:int=1):
         """
-            This class takes nn.Conv2d module, makes it non-trainable and adds a trainable lora layer initialized to zero 
+            This class takes nn.Conv2d module, makes it non-trainable and adds a trainable lora layer initialized to zero
+
+            all in all in about 50% slower than the conv_layer 
         """
 
         super().__init__()
@@ -16,20 +18,24 @@ class LoraConvLayer(nn.Module):
         self.in_channels = conv_layer.in_channels
         self.out_channels = conv_layer.out_channels
         self.kernel_size = conv_layer.kernel_size
-        self.device=conv_layer.device
+        #self.device=conv_layer.device
 
-        assert type(rank)==1, f"rank must be an integer, got {type(rank)} instead"
+        assert type(rank)==int, f"rank must be an integer, got {type(rank)} instead"
         assert rank>0, f"rank must be positive, got {rank} instead"
         assert rank<min(self.in_channels,self.out_channels), f"why do you even use a rank that gives you more parameters than you started with lol"
         
-        self.factor_matrix1=nn.Parameter(torch.zeros(self.in_channels,rank))
-        self.factor_matrix2=nn.Parameter(torch.zeros(rank, self.out_channels))
+        self.factor_matrix1=nn.Parameter(torch.zeros(self.out_channels,rank))
+        self.factor_matrix2=nn.Parameter(torch.zeros(rank, self.in_channels))
+        self.factor_bias=nn.Parameter(torch.zeros(rank))
 
-        self.to(conv_layer.device)
-        conv_layer.no_grad()
+        #self.to(conv_layer.device)
+        conv_layer.weight.requires_grad=False
+        conv_layer.bias.requires_grad=False
 
     def forward(self, images):
 
-        weight=conv_layer
+        weight=self.conv_layer.weight + (self.factor_matrix1@self.factor_matrix2)[...,None,None]
+        bias=self.conv_layer.bias + (self.factor_matrix1@self.factor_bias)
+
 
         F.conv2d(images,weight,bias)
