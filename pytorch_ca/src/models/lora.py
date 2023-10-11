fixed gradient lora bug
By initializing the weights to zero, the gradient was also zero

pytorch_ca/src/models/lora.py
@@ -1,12 +1,8 @@
-from typing import Iterator
 import torch
 from torch import nn
 
 from torch.nn import functional as F
-from torch.nn.parameter import Parameter
-from src.models.multiple_CA import CustomCA
 
-from src.models.neural_CA import NeuralCA
 
 """
 TODO:
@@ -17,10 +13,14 @@ Make make sure that the original weights remain unaltered during training
 
 class LoraConvLayer(nn.Module):
     def __init__(self, conv_layer:nn.Conv2d, rank:int=1):
-        """
-            This class takes nn.Conv2d module, makes it non-trainable and adds a trainable lora layer initialized to zero
+        """This class takes nn.Conv2d module, makes it non-trainable and adds a trainable lora layer initialized to zero. 
+        All in all in about 50% slower than the conv_layer 
 
-            all in all in about 50% slower than the conv_layer 
+        When initialized, it makes sure that the parameters of the conv_layer are not altered during training
+
+        Args:
+            conv_layer (nn.Conv2d): nn.Conv2d module to be wrapped
+            rank (int, optional): rank of the lora layer. Defaults to 1.        
         """
 
         super().__init__()
@@ -34,24 +34,25 @@ class LoraConvLayer(nn.Module):
         assert type(rank)==int, f"rank must be an integer, got {type(rank)} instead"
         assert rank>0, f"rank must be positive, got {rank} instead"
         assert rank<min(self.in_channels,self.out_channels), f"why do you even use a rank that gives you more parameters than you started with lol"
-        
-        self.factor_matrix1=nn.Parameter(torch.zeros(self.out_channels,rank))
-        self.factor_matrix2=nn.Parameter(torch.zeros(rank, self.in_channels))
-        self.factor_bias=nn.Parameter(torch.zeros(rank))
+
+        #TODO: Improve initialization 
+        self.factor_matrix1=nn.Parameter(torch.randn(self.out_channels,rank))
+        self.factor_matrix2=nn.Parameter(torch.randn(rank, self.in_channels))
+        self.factor_bias=nn.Parameter(torch.randn(rank))
 
         conv_layer.weight.requires_grad=False
         conv_layer.bias.requires_grad=False
 
-        self.to(conv_layer.weight.device)
+        self.device=conv_layer.weight.device
+        self.to(self.device)
 
-    def forward(self, images):
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
 
-        self.lora_weight=(self.factor_matrix1@self.factor_matrix2)[...,None,None]
-        self.lora_bias= self.factor_matrix1@self.factor_bias        
+        self.lora_weight = (self.factor_matrix1 @ self.factor_matrix2)[...,None,None]
+        self.lora_bias = self.factor_matrix1 @ self.factor_bias        
         
-        weight=self.conv_layer.weight + self.lora_weight
-        bias=self.conv_layer.bias + self.lora_bias
+        weight = self.conv_layer.weight + self.lora_weight
+        bias = self.conv_layer.bias + self.lora_bias
 
 
         return F.conv2d(images,weight,bias)
-

pytorch_ca/src/models/neural_CA.py
@@ -6,7 +6,7 @@ import os.path
 from ..utils import *
 from ..sample_pool import *
 from .CAModel import *
-from src import LoraConvLayer
+from .lora import LoraConvLayer
 class Perciever(nn.Module): #forse non serve ereditare da nn.Module
     def __init__(self, n_channels:int, device:torch.device, angle:float=0.):
         super().__init__()
@@ -139,7 +139,7 @@ class NeuralCA(CAModel):
             fname (str): Path of the model to load
         """
 
-        self.load_state_dict(torch.load(fname))
+        self.load_state_dict(torch.load(fname, map_location=self.device))
         print("Successfully loaded model!")
 
     def save(self, fname: str, overwrite: bool = False):